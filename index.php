fa67b6cd962accdc29b7e8b9755c439520bd27ff
Marcel Poloczek <marcolp0@wp.pl>
2020-05-29 12:35:40 +0200

    sample routing

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    echo "test";
-?>
+switch ($_SERVER['REQUEST_URI']) {
+    case '/':
+        require_once('strona-glowna.php');
+        break;
+    
+    case '/kontakt':
+        require_once('kontakt.php');
+        break;    
+
+    default:
+        require_once('404.php');
+        break;
+    }
