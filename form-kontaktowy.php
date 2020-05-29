092b38f8161eedffb205a6341a8fbf17b4338038
Marcel Poloczek <marcolp0@wp.pl>
2020-05-29 13:32:19 +0200

    form

--- form-kontaktowy.php
+++ form-kontaktowy.php
@@ -1,9 +1,26 @@
+<form name="Kontakt" method="POST" >
+		Tytul :<input type="text" name="im" ><br>
+		Treść :<textarea name="in"></textarea><br>
+		
+		<input type="submit">
+	</form>
 <?php
+
+if($_POST){
+    $nazwa=$_POST['im'];
+    $data=date("Y-m-d");
+    $czas=date("H-i-s");
+    $file=fopen("$data-$czas-$nazwa.txt","w");
+    fputs($file, $_POST["in"]);
+    fclose($file);
+}
+
+
+
+
+
 /**
  * Utwórz formularz kontaktowy na stronie localhost/kontakt który zostanie wysłany na skrypt wyslij-formularz.php który spowoduje zapisanie treści formularza 
  * do pliku w katalogu formularze/ o formacie YYYY-mm-dd-HH-ii-ss-temat-formularza.txt
  * Formularz powinien zawierać takie pola jak temat oraz treść, oraz przycisk wyslij który spowoduje wysyłkę formularza
  */
-include_once ("form-kontaktowy.php");
-
-?>
