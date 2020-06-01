f3a64aba2947f83aa08b6426694bbecd45f38ba9
Marcel Poloczek <marcolp0@wp.pl>
2020-06-01 23:25:47 +0200

    hash

--- form-kontaktowy.php
+++ form-kontaktowy.php
@@ -11,7 +11,7 @@ if($_POST){
 $fileName = \sprintf(
     'forms/%s-%s.txt',
     \date('Y-m-d-H-i-s'),
-    \substr(\md5($_POST["title"], 0, 8)
+    \substr(\md5($_POST["title"]), 0, 8)
 );
     \file_put_contents($fileName, $_POST['content']);
 }
