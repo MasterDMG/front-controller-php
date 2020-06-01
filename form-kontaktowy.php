4e4cb805a993c7d48da321f732f6fd4863372f52
Paweł Poloczek <30537984+pawelpoloczek@users.noreply.github.com>
2020-06-01 23:14:40 +0200

    8 chars hash instead title

--- form-kontaktowy.php
+++ form-kontaktowy.php
@@ -1,36 +1,15 @@
 <?php
 
 /**
- * Zamiast tytułu użyj algorytmu hashującego (np md5) aby ostatni czlon nazwy pliku był 8 cyfrowym hashem
+ * Zamiast tytułu użyj algorytmu hashującego (np md5) aby ostatni człon nazwy pliku był 8 cyfrowym hashem
  */
 
 if($_POST){
     
-    
-function szyfruj($k, &$title ){
-	$length=strlen($title); 
-	if($k >= 0)
-		for($i=0;$i<$length;$i++){
-			$x=ord($title[$i]);
-		    if($x+$k<=ord('Z')){
-				$x+=$k;
-				$title[$i]=chr($x);
-			}
-			else {
-				$x=$x+$k-26;
-				$title[$i]=chr($x);
-			}
-		}
-	else
-		for($i=0;$i<$length;$i++)
-		    if($title[$i]+$k>='A')$title[$i]+=$k;
-		    else $title[$i] = $title[$i] + $k + 26;
-    return $title;
-    }
 $fileName = \sprintf(
     'forms/%s-%s.txt',
     \date('Y-m-d-H-i-s'),
-    szyfruj(10, $_POST["title"])
+    \substr(\md5($_POST["title"], 0, 8)
 );
     \file_put_contents($fileName, $_POST['content']);
 }
