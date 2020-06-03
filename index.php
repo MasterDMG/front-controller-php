afddc9b5b51e8be60abc90fc1a55881d3bb3ae42
Marcel Poloczek <marcolp0@wp.pl>
2020-06-03 22:45:24 +0200

    New Index

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
-<?php
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        require_once('home.php');
-        break;
-    
-    case '/kontakt':
-        require_once('contact.php');
-        break;    
-
-    case '/wyslij-formularz':
-        require_once('contact_form.php');
-        break;
-
-    default:
-        require_once('404.php');
-        break;
-    }
+<?php 
+$URL = array('/' =>'home.php',
+'/kontakt' =>'contact.php',
+'/wyslij-formularz' =>'contact-form.php'
+);
+require_once($URL[$_SERVER['REQUEST_URI']]);
