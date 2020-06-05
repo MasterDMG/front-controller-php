26ff94b73c72b2c7516e2926316704d3f1fe9410
Marcel Poloczek <marcolp0@wp.pl>
2020-06-05 16:02:23 +0200

    forms

--- form.php
+++ form.php
@@ -2,8 +2,15 @@
 
 $file = $_SERVER['REQUEST_URI'] . '.txt';
 $content = file_get_contents('forms/.'.$file);
+$i=0;
+while($content[$i]!='<'){
+    $title.=$content[$i];
+    $i++;
+    
+}
+$content=\substr($content, $i+4);
 
-$title = '';
+
 echo '<h1>'.$title.'</h1>';
 
 echo $content;
