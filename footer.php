9075ec4defba41456374fd6c4d7b71c518eb3528
Pawel Poloczek <pawel@poloczek.me>
2020-06-15 21:44:12 +0200

    Some styles

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-    </div>
-    <div id="footer">
-    
-    </div>
+                </div>
+                <div id="footer" class="bg-teal-700 row-span-1 col-span-3 p-10">Simple CMS <?php echo date('Y'); ?></div>
+            </div>
+        </div>
     </body>
 </html>
--- header.php
+++ header.php
@@ -5,27 +5,27 @@
         <link rel="stylesheet" href="/css/styles.css">  
     </head>
     <body>
-    <div id="main">
-            <h5>NAGŁÓWEK</h5>
-    </div>
-    <div id="menu">
-        <a href='/'>STRONA GŁÓWNA</a><br>
-        <a href='/kontakt'>KONTAKT</a><br>
-        <a href='/formularze'>FORMULARZE</a><br>
-        <a href='/artykuly'>ARTYKUŁY</a><br>
-        <a href='/rejestracja'>ZAREJESTRUJ</a><br>
-        <?php
-            echo'<a href="/sprawdz">DODAJ ARTYKUL</a><br>';
-            if(!isset($_SESSION['user'])){
-                echo"<a href='/zaloguj'>ZALOGUJ</a><br>";
-            
-            }
-            else {
-                echo"<a href='/wyloguj'>WYLOGUJ</a><br>";
-            }
-        ?>
-    </div>
-    <div id="content">
+    <div class="container mx-auto bg-teal-500">
+        <div class="grid grid-rows-4 grid-flow-col gap-4">
+            <div id="menu" class="bg-teal-800 row-span-4 p-10">
+                <a href='/'>STRONA GŁÓWNA</a><br>
+                <a href='/kontakt'>KONTAKT</a><br>
+                <a href='/formularze'>FORMULARZE</a><br>
+                <a href='/artykuly'>ARTYKUŁY</a><br>
+                <a href='/rejestracja'>ZAREJESTRUJ</a><br>
+                <?php
+                    echo'<a href="/sprawdz">DODAJ ARTYKUL</a><br>';
+                    if(!isset($_SESSION['user'])) {
+                        echo"<a href='/zaloguj'>ZALOGUJ</a><br>";
+                    } else {
+                        echo"<a href='/wyloguj'>WYLOGUJ</a><br>";
+                    }
+                ?>
+            </div>
+            <div id="main" class="bg-teal-700 row-span-1 col-span-3 p-10">
+                    <h5>NAGŁÓWEK</h5>
+            </div>
+            <div id="content" class="bg-teal-600 row-span-2 col-span-3 p-10">
 <?php
 
 if (isset($_SESSION['messages'])) {
